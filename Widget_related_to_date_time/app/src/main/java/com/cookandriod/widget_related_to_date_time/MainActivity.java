7b7349639510c11b1f6a63d113aece1152577a7e
suleun <teaaa030@gmail.com>
2021-10-05 11:48:52 +0900

    Widget_related_to_date_time

---
+++ Widget_related_to_date_time/app/src/main/java/com/cookandriod/widget_related_to_date_time/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cookandriod.widget_related_to_date_time;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
