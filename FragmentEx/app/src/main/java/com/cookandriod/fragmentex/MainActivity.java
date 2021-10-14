aa3535039e57eaa2d9efa75ab88f505f686d5d3f
suleun <teaaa030@gmail.com>
2021-10-14 12:44:33 +0900

    Fragment

---
+++ FragmentEx/app/src/main/java/com/cookandriod/fragmentex/MainActivity.java
@@ -0,0 +1,34 @@
+package com.cookandriod.fragmentex;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void setFrag1(View v){
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction ft = manager.beginTransaction();
+
+        ft.replace(R.id.frame_container, new fragment1(), "one");
+        ft.commitAllowingStateLoss();
+    }
+
+    public void setFrag2(View v){
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction ft = manager.beginTransaction();
+
+        ft.replace(R.id.frame_container, new fragment2(), "two");
+        ft.commitAllowingStateLoss();
+    }
+}
