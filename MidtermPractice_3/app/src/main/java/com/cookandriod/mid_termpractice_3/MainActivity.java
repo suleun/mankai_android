26147607d4274bf55d4d3f5dd25c8576e1086c9c
suleun <teaaa030@gmail.com>
2021-10-18 19:33:28 +0900

    Solving it by myself.

---
+++ MidtermPractice_3/app/src/main/java/com/cookandriod/mid_termpractice_3/MainActivity.java
@@ -0,0 +1,25 @@
+package com.cookandriod.mid_termpractice_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edt1, edt2;
+    Button [] btns ;
+    TextView resultTV;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edt1 = (EditText)findViewById(R.L.edt1);
+
+
+    }
+}
