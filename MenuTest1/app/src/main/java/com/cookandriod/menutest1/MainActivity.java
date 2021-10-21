1d075a7d53c122dceff48f8ba0e12462474b6b88
suleun <teaaa030@gmail.com>
2021-10-21 12:29:49 +0900

    ContextMenu

--- MenuTest1/app/src/main/java/com/cookandriod/menutest1/MainActivity.java
+++ MenuTest1/app/src/main/java/com/cookandriod/menutest1/MainActivity.java
@@ -5,15 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn1;
+    Button btn1, btn2;
     LinearLayout baseLayout;
     int rotation = 45;
 
@@ -23,19 +25,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btn1 = (Button)findViewById(R.id.btn1);
+        btn2 = (Button)findViewById(R.id.btn2);
         baseLayout = (LinearLayout)findViewById(R.id.baseLayout);
+
+        setTitle("배경색 바꾸기");
+
+        registerForContextMenu(btn1);
+        registerForContextMenu(btn2);
+
     }
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
         MenuInflater mInflater = getMenuInflater();
-        mInflater.inflate(R.menu.menu, menu);
-        return true;
+       if (v == btn1){
+           mInflater.inflate(R.menu.menu, menu);
+       } else if(v == btn2){
+           mInflater.inflate(R.menu.menu2, menu);
+       }
     }
 
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        MenuInflater mInflater = getMenuInflater();
+//        mInflater.inflate(R.menu.menu, menu);
+//        return true;
+//    }
+
+
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+
         switch (item.getItemId()){
             case R.id.itemRed:
                 baseLayout.setBackgroundColor(Color.RED);
@@ -50,19 +73,57 @@ public class MainActivity extends AppCompatActivity {
                 return true;
 
             case R.id.subRotate:
-                btn1.setRotation(rotation);
+                btn2.setRotation(rotation);
                 rotation = rotation + 45 ;
                 return true;
 
             case R.id.subRotate0:
-                btn1.setRotation(0);
+                btn2.setRotation(0);
+
+                btn2.setScaleX(1);
+                btn2.setScaleY(1);
                 return true;
 
             case R.id.subSize:
-                btn1.setScaleX(2);
-                btn1.setScaleY(2);
+                btn2.setScaleX(2);
+                btn2.setScaleY(2);
                 return true;
         }
         return false;
+
     }
+
+
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()){
+//            case R.id.itemRed:
+//                baseLayout.setBackgroundColor(Color.RED);
+//                return true;
+//
+//            case R.id.itemGreen:
+//                baseLayout.setBackgroundColor(Color.GREEN);
+//                return true;
+//
+//            case R.id.itemBlue:
+//                baseLayout.setBackgroundColor(Color.BLUE);
+//                return true;
+//
+//            case R.id.subRotate:
+//                btn1.setRotation(rotation);
+//                rotation = rotation + 45 ;
+//                return true;
+//
+//            case R.id.subRotate0:
+//                btn1.setRotation(0);
+//                return true;
+//
+//            case R.id.subSize:
+//                btn1.setScaleX(2);
+//                btn1.setScaleY(2);
+//                return true;
+//        }
+//        return false;
+//    }
 }
