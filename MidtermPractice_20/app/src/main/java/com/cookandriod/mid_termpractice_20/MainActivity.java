d437368b91b39f8815b830cf1b67c3a1ac696321
suleun <teaaa030@gmail.com>
2021-10-25 16:45:56 +0900

    Mid-termPractice_20-1, The code description is in the ppt file.

--- MidtermPractice_20/app/src/main/java/com/cookandriod/mid_termpractice_20/MainActivity.java
+++ MidtermPractice_20/app/src/main/java/com/cookandriod/mid_termpractice_20/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.SubMenu;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -22,28 +23,38 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
-        MenuInflater mInflater = getMenuInflater();
-        mInflater.inflate(R.menu.menu, menu);
+//        MenuInflater mInflater = getMenuInflater();
+//        mInflater.inflate(R.menu.menu, menu);
+
+        menu.add(0,1,0,"배경색 빨강");
+        menu.add(0,2,0,"배경색 초록");
+        menu.add(0,3,0,"배경색 파랑");
+
+        SubMenu sMenu = menu.addSubMenu("버튼변경");
+        sMenu.add(0,4,0,"버튼 2배 확대");
+        sMenu.add(0,5,0,"버튼45도 회전");
+
+
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()){
-            case R.id.red:
+            case 1:
                 baseLayout.setBackgroundColor(Color.RED);
                 return true;
-            case R.id.green:
+            case 2:
                 baseLayout.setBackgroundColor(Color.GREEN);
                 return true;
-            case R.id.blue:
+            case 3:
                 baseLayout.setBackgroundColor(Color.BLUE);
                 return true;
-            case R.id.btn2:
+            case 4:
                 btn.setScaleX(2);
                 btn.setScaleY(2);
                 return true;
-            case R.id.btnR:
+            case 5:
                 btn.setRotation(rotation);
                 rotation = rotation + 45;
         }
