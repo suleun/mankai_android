7df23087fa07de5cc743d6989c9ba422026f1fee
suleun <teaaa030@gmail.com>
2021-10-25 17:21:27 +0900

    Mid-termPractice_20-2, Context Menu

--- MidtermPractice_20/app/src/main/java/com/cookandriod/mid_termpractice_20/MainActivity.java
+++ MidtermPractice_20/app/src/main/java/com/cookandriod/mid_termpractice_20/MainActivity.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.style.BackgroundColorSpan;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.SubMenu;
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -16,62 +19,64 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     LinearLayout baseLayout;
-    Button btn;
-    Integer rotation = 45;
+    Button btn1, btn2;
+    Integer rotation = 0;
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-//        MenuInflater mInflater = getMenuInflater();
-//        mInflater.inflate(R.menu.menu, menu);
-
-        menu.add(0,1,0,"배경색 빨강");
-        menu.add(0,2,0,"배경색 초록");
-        menu.add(0,3,0,"배경색 파랑");
-
-        SubMenu sMenu = menu.addSubMenu("버튼변경");
-        sMenu.add(0,4,0,"버튼 2배 확대");
-        sMenu.add(0,5,0,"버튼45도 회전");
-
-
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
-            case 1:
-                baseLayout.setBackgroundColor(Color.RED);
-                return true;
-            case 2:
-                baseLayout.setBackgroundColor(Color.GREEN);
-                return true;
-            case 3:
-                baseLayout.setBackgroundColor(Color.BLUE);
-                return true;
-            case 4:
-                btn.setScaleX(2);
-                btn.setScaleY(2);
-                return true;
-            case 5:
-                btn.setRotation(rotation);
-                rotation = rotation + 45;
-        }
-        return false;
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setTitle("메뉴");
+        setTitle("컨텍스트 메뉴");
 
 
         baseLayout = findViewById(R.id.baseLayout);
-        btn = findViewById(R.id.btn);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+
+        registerForContextMenu(btn1);
+        registerForContextMenu(btn2);
 
 
     }
+
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        MenuInflater mInflater = getMenuInflater();
+
+        if (v == btn1) {
+            mInflater.inflate(R.menu.menu, menu);
+        } else if (v == btn2) {
+            mInflater.inflate(R.menu.menu1, menu);
+        }
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.red:
+                baseLayout.setBackgroundColor(Color.RED);
+                return true;
+            case R.id.green:
+                baseLayout.setBackgroundColor(Color.GREEN);
+                return true;
+            case R.id.blue:
+                baseLayout.setBackgroundColor(Color.BLUE);
+                return true;
+            case R.id.btnR:
+                rotation = rotation + 45;
+                btn2.setRotation(rotation);
+                return true;
+            case R.id.btnS:
+                btn2.setScaleX(2);
+                btn2.setScaleY(2);
+                return true;
+        }
+        return false;
+    }
 }
+
