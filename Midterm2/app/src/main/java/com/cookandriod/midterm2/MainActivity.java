616aed445f147122c1cc0c8f69a5d07d28f070ce
suleun <teaaa030@gmail.com>
2021-10-26 13:09:34 +0900

    Midterm

---
+++ Midterm2/app/src/main/java/com/cookandriod/midterm2/MainActivity.java
@@ -0,0 +1,135 @@
+package com.cookandriod.midterm2;
+
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.Chronometer;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    //전역변수 선언
+    Chronometer chrono;
+    Button btnStart, btnEnd;
+    RadioButton rdoCal, rdoTime;
+    CalendarView calView;
+    TimePicker tPicker;
+    TextView tvYear, tvMonth, tvDay, tvHour, tvMinute;
+    int selectYear, selectMonth, selectDay;
+    int hourV, mint;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setTitle("시간 예약");
+
+        // 버튼
+        btnStart = (Button) findViewById(R.id.btnStart);
+        btnEnd = (Button) findViewById(R.id.btnEnd);
+
+        // 크로노미터
+        chrono = (Chronometer) findViewById(R.id.chronometer1);
+
+        // 라디오버튼 2개
+        rdoCal = (RadioButton) findViewById(R.id.rdoCal);
+        rdoTime = (RadioButton) findViewById(R.id.rdoTime);
+
+        // FrameLayout의 2개 위젯
+        tPicker = (TimePicker) findViewById(R.id.timePicker1);
+        calView = (CalendarView) findViewById(R.id.calendarView1);
+
+        // 텍스트뷰 중에서 연,월,일,시,분 숫자
+        tvYear = (TextView) findViewById(R.id.tvYear);
+        tvMonth = (TextView) findViewById(R.id.tvMonth);
+        tvDay = (TextView) findViewById(R.id.tvDay);
+        tvHour = (TextView) findViewById(R.id.tvHour);
+        tvMinute = (TextView) findViewById(R.id.tvMinute);
+
+        // 처음에는 타임 피커와 캘린더뷰 2개를 안보이게 설정하는 코드 입력
+        tPicker.setVisibility(View.INVISIBLE);
+        calView.setVisibility(View.INVISIBLE);
+
+
+        //<날짜 설정> 라디오 버튼 클릭하면 캘린더만 보이도록 설정
+        rdoCal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calView.setVisibility(View.VISIBLE);
+                tPicker.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        //<시간 설정> 라디오 버튼 클릭하면 타임피커만 보이도록 설정
+
+        rdoTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                calView.setVisibility(View.INVISIBLE);
+                tPicker.setVisibility(View.VISIBLE);
+            }
+        });
+
+        // <예약 시작> 버튼 클릭하면 크로노미터 초기화 및 시작
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chrono.getFormat();
+                chrono.start();
+            }
+        });
+
+        // <캘린더 뷰>에서 날짜를 선택하면 예약된 연, 월, 일이 전역 변수에 대입된다.
+        calView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int date) {
+                selectYear = year;
+                selectMonth = month;
+                selectDay = date;
+            }
+        });
+
+        tPicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker timePicker, int hour, int min) {
+                hourV = hour;
+                mint = min;
+            }
+        });
+
+
+        // <예약 완료> 버튼을 클릭하면 크로노미터 정지 . 날짜, 시간 텍스트뷰로 예약한 날짜,시간을 보여준다.
+        btnEnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chrono.stop();
+                chrono.setTextColor(Color.BLUE);
+
+                tvYear.setText(selectYear);
+                tvMonth.setText(selectMonth);
+                tvDay.setText(selectDay);
+                tvHour.setText(hourV);
+                tvMinute.setText(mint);
+
+            }
+        });
+
+    }
+
+}
