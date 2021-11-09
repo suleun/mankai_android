9bc8e8ec79943dd6d5578391b734f0f943ce1e62
suleun <teaaa030@gmail.com>
2021-11-09 12:20:41 +0900

    intent

---
+++ pro10_1/app/src/main/java/com/cookandriod/pro10_1/thirdActivity.java
@@ -0,0 +1,26 @@
+package com.cookandriod.pro10_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class thirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.third);
+
+        Button btnReturn = findViewById(R.id.btnReturn);
+        btnReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
