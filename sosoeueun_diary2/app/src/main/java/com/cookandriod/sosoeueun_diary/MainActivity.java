97f0679ae828b3b641fa6f90492762f3806738a7
suleun <teaaa030@gmail.com>
2021-12-16 17:08:08 +0900

    make todo list

--- sosoeueun_diary2/app/src/main/java/com/cookandriod/sosoeueun_diary/MainActivity.java
+++ sosoeueun_diary2/app/src/main/java/com/cookandriod/sosoeueun_diary/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setTitle("일기장");
+        setTitle("Diary");
 
 
         menu = findViewById(R.id.menu);
--- sosoeueun_diary2/app/src/main/java/com/cookandriod/sosoeueun_diary/emotion.java
+++ sosoeueun_diary2/app/src/main/java/com/cookandriod/sosoeueun_diary/emotion.java
@@ -8,7 +8,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.ListView;
 
@@ -25,13 +28,33 @@ public class emotion extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_emotion, container, false);
 
-        String[] menuItems = {"number one", "number two", "number three", "number four"};
+        final ArrayList<String> midList = new ArrayList<String>();
+        ListView list = (ListView) view.findViewById(R.id.listView);
 
-        ListView listView = (ListView) view.findViewById(R.id.listView);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, midList);
+        list.setAdapter(adapter);
 
-        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, menuItems);
+        final EditText edtItem = (EditText) view.findViewById(R.id.inputToDo);
+        Button btnAdd = (Button) view.findViewById(R.id.btnAdd);
 
-        listView.setAdapter(listViewAdapter);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                midList.add(edtItem.getText().toString());
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long id) {
+
+                midList.remove(position);
+                adapter.notifyDataSetChanged();
+
+                return false;
+            }
+        });
 
         return view;
 
