99126395cebbee089955541c37cb354952d889c9
suleun <teaaa030@gmail.com>
2022-04-06 10:25:07 +0900

    bottom nav bar

---
+++ mankai_page/app/src/main/java/com/cookandriod/mankai_page/MainActivity.java
@@ -0,0 +1,21 @@
+package com.cookandriod.mankai_page;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigation = findViewById(R.id.bottomNavigation);
+        bottomNavigation.setBackground(null);
+    }
+}
