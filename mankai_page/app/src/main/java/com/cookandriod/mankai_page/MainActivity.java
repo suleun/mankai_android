1fb6731cf5ffa8b7a9b41a8f7a840eb00b7888dd
suleun <teaaa030@gmail.com>
2022-04-07 11:36:53 +0900

    my page fragment tap layout

--- mankai_page/app/src/main/java/com/cookandriod/mankai_page/MainActivity.java
+++ mankai_page/app/src/main/java/com/cookandriod/mankai_page/MainActivity.java
@@ -1,21 +1,80 @@
 package com.cookandriod.mankai_page;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 
 public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigation;
+    LinearLayout  page;
+    FloatingActionButton chatting;
 
+    private Fragment settingFragment, groupFragment, postFragment, mypageFragment;
+    private Fragment chattingFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        page = findViewById(R.id.page);
         bottomNavigation = findViewById(R.id.bottomNavigation);
         bottomNavigation.setBackground(null);
+
+        chatting = findViewById(R.id.chatting);
+
+        bottomNavigation.setOnNavigationItemSelectedListener(listener);
+
+        postFragment = new PostFragment();
+        groupFragment = new GroupFragment();
+        mypageFragment = new MypageFragment();
+        settingFragment = new SettingFragment();
+
+        chattingFragment = new ChattingFragment();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.page, postFragment).commit();
+
+        chatting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.page, chattingFragment).commit();
+            }
+
+        });
+
+
     }
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener listener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()){
+                case R.id.post:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.page, postFragment).commit();
+                    return true;
+                case R.id.group:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.page, groupFragment).commit();
+                    return true;
+                case R.id.mypage:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.page, mypageFragment).commit();
+                    return true;
+                case R.id.setting:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.page, settingFragment).commit();
+                    return true;
+            }
+            return false;
+        }
+    };
 }
+
+
---
+++ mankai_page/app/src/main/java/com/cookandriod/mankai_page/Mypage_followerFragment.java
@@ -0,0 +1,28 @@
+package com.cookandriod.mankai_page;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Mypage_followerFragment extends Fragment {
+
+
+    public Mypage_followerFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+
+}
